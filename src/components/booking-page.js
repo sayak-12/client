b2fb3564bfd6daa1d4d6182a182bbfcafd261439
Taazatv2 <taazatv2@gmail.com>
2024-09-24 15:54:29 +0530

    ticekts headline

--- src/components/booking-page.js
+++ src/components/booking-page.js
@@ -5,6 +5,7 @@ export default function BookingPage(){
         <div className="h-screen w-screen overflow-hidden flex flex-col gap-4 p-6 py-6" id="ticket-booking-form ">
             <ToastContainer />
             <h1 className="text-center text-4xl font-bold text-pink-800 drop-shadow-2xl">Taaza Dandiya 2024 Ticket Booking</h1>
+            <h3 className="text-center text-xl font-bold text-pink-800 drop-shadow-2xl">Tickets at Rs.700 (inc tax) each. </h3>
             <div className="m-auto overflow-y-auto">
             <BookingForm />
             </div>
