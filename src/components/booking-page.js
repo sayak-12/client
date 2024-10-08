caa175b0a1143f35b8ca10d7f97b0b66bb2b251e
Taazatv2 <taazatv2@gmail.com>
2024-10-08 17:13:49 +0530

    remove early bird

--- src/components/booking-page.js
+++ src/components/booking-page.js
@@ -6,7 +6,8 @@ export default function BookingPage(){
             <ToastContainer />
             <h1 className="text-center text-4xl font-bold text-pink-800 drop-shadow-2xl">Taaza Dandiya 2024 Ticket Booking</h1>
             <h3 className="text-center text-xl font-bold text-pink-800 drop-shadow-2xl">Tickets at Rs.700 (incl. tax) each. </h3>
-            <h3 className="text-center text-xl font-bold text-black drop-shadow-2xl">Early bird offer closes on 7th Oct 2024 midnight</h3>
+            <div className="BOX">
+             </div>
             <div className="m-auto overflow-y-auto">
             <BookingForm />
             </div>
